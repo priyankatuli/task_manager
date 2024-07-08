668725f4e695bed4166f7fc7b3de730e4427ac90
priyankatuli <priyankaaatuli123@gmail.com>
2024-07-08 20:34:24 +0600

    InProgress task API call

--- lib/ui/screens/in_progress_screen.dart
+++ lib/ui/screens/in_progress_screen.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:task_manager/UI/widgets/task_items.dart';
+import 'package:task_manager/UI/widgets/task_item.dart';
+import 'package:task_manager/data/model/api_response.dart';
+import 'package:task_manager/data/model/task_list_wrapper_model.dart';
+import 'package:task_manager/data/model/task_model.dart';
+import 'package:task_manager/data/network_caller/api_call.dart';
+import 'package:task_manager/ui/utility/url_list.dart';
+import 'package:task_manager/ui/widgets/centered_progress_Indicator.dart';
+import 'package:task_manager/ui/widgets/snack_bar_message.dart';
 
 
 class InProgressScreen extends StatefulWidget{
@@ -14,18 +21,83 @@ class InProgressScreen extends StatefulWidget{
 }
 
 class _NewTaskScreenState extends State<InProgressScreen>{
+
+
+  bool _inProgress = false;
+
+  List<TaskModel> inProgressTaskList = [];
+
+
+  @override
+  void initState(){
+    super.initState();
+    _getInProgress();
+
+
+  }
+
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
-      body: ListView.builder(
-        itemBuilder: (context,index){
-          return const TaskItem();
+      body: RefreshIndicator(
+        onRefresh: ()async{
+          await _getInProgress();
         },
-        itemCount: 5,
-      ),
+        child: Visibility(
+          visible: _inProgress == false,
+            replacement: const CenteredProgressIndicator(),
+            child:  ListView.builder(
+              itemBuilder: (context,index){
+                return TaskItem(
+                  taskModel: inProgressTaskList[index],
+                  onUpdateTask: (){
+                      _getInProgress();
+                  },
+                );
+              },
+              itemCount: inProgressTaskList.length,
+            ),
+        ),
+      )
+
+
     );
 
   }
+
+  Future<void> _getInProgress()async{
+    _inProgress = true;
+    if(mounted){
+      setState(() {
+        
+      });
+    }
+    
+    ApiResponse response = await ApiCall.getResponse(UrlList.inProgressTask);
+
+    if(response.isSuccess){
+      TaskListWrapperModel taskListWrapperModel = TaskListWrapperModel.fromJson(response.responseData);
+      inProgressTaskList = taskListWrapperModel.task ?? [];
+    }
+    else{
+      if(mounted){
+        showSnackBarMessage(context, response.errorMessage ?? 'In progress task failed!!please try again');
+      }
+    }
+
+    _inProgress = false;
+    if(mounted){
+      setState(() {
+
+      });
+
+    }
+
+    
+    
+  }
+
+
 
 }
