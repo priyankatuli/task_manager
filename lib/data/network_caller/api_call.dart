631f07a78ff5a1298656b2a9d6d918d09fca395c
priyankatuli <priyankaaatuli123@gmail.com>
2024-07-08 20:31:21 +0600

    main network caller class

--- lib/data/network_caller/api_call.dart
+++ lib/data/network_caller/api_call.dart
@@ -1,13 +1,22 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'package:task_manager/UI/screens/authentication/sign_in_screen.dart';
+import 'package:task_manager/app.dart';
 import 'package:task_manager/data/model/api_response.dart';
 import 'package:task_manager/ui/controllers/authentication_controller.dart';
 
 class ApiCall {
+
   static Future<ApiResponse> getResponse(String url) async {
     try {
+
+      debugPrint(url);
       Response serverResponse = await get(Uri.parse(url),
-          headers: {'token':AuthenticationController.accessToken});
+          headers: {'token': AuthenticationController.accessToken});
+
+      debugPrint(serverResponse.statusCode.toString());
+      debugPrint(serverResponse.body);
 
       if (serverResponse.statusCode == 200) {
         final dynamic serverResponseData = jsonDecode(serverResponse.body);
@@ -16,7 +25,14 @@ class ApiCall {
             isSuccess: true,
             responseData: serverResponseData,
             errorMessage: null);
-      } else {
+      } else if(serverResponse.statusCode == 401) {
+        redirectToLogin();
+        return ApiResponse(
+            statusCode: serverResponse.statusCode,
+            isSuccess: false
+        );
+      }
+      else{
         return ApiResponse(
             statusCode: serverResponse.statusCode,
             isSuccess: false
@@ -31,27 +47,37 @@ class ApiCall {
     }
   }
 
-  static Future<ApiResponse> postResponse(
-      String url, Map<String, dynamic>? body) async {
+  static Future<ApiResponse> postResponse(String url,
+      Map<String, dynamic>? body) async {
     try {
+      debugPrint(url);
       Response serverResponse = await post(Uri.parse(url),
           body: jsonEncode(body),
-          headers: {'content-type':'application/json',
-            'token':AuthenticationController.accessToken});
-      if (serverResponse.statusCode == 200 ||
-          serverResponse.statusCode == 201) {
+          headers: {'content-type': 'application/json',
+            'token': AuthenticationController.accessToken});
+
+      debugPrint(serverResponse.statusCode.toString());
+      debugPrint(serverResponse.body);
+
+      if (serverResponse.statusCode == 200 || serverResponse.statusCode == 201) {
         final dynamic serverResponseData = jsonDecode(serverResponse.body);
         return ApiResponse(
             statusCode: serverResponse.statusCode,
             isSuccess: true,
             responseData: serverResponseData,
             errorMessage: null);
-      } else {
+      } else if(serverResponse.statusCode == 401){
+        redirectToLogin();
         return ApiResponse(
             statusCode: serverResponse.statusCode,
             isSuccess: false
         );
       }
+    else{
+         return ApiResponse(
+              statusCode: serverResponse.statusCode,
+              isSuccess: false);
+    }
     } catch (e) {
       return ApiResponse(
         statusCode: -1,
@@ -60,4 +86,16 @@ class ApiCall {
       );
     }
   }
+
+  static Future<void> redirectToLogin()async{
+
+    await AuthenticationController.clearAllData();
+    Navigator.pushAndRemoveUntil(
+        TaskManagerApp.navigatorKey.currentContext!,
+        MaterialPageRoute(builder: (context) => const SignInScreen()),
+            (route) => false
+    );
+  }
+
+
 }
